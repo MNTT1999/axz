907a2fd27c081b7872d30a26fe38bf60f9c81b15
MNTT1999 <94632489+MNTT1999@users.noreply.github.com>
2021-12-11 18:35:29 +0800

    Update mxmxcn.sh

--- mxmxcn.sh
+++ mxmxcn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
-tar xfvz xmrig-6.15.3-linux-x64.tar.gz
-cd xmrig-6.15.3
-./xmrig -o pool.supportxmr.com:443 -u 4AQgUnhWMAfGHFtuoibTL5U8f5SD62ZpB1u3YrwkB7ngSd57Rgs5rMqKKdDvyh5egnWipwTv3pRqc7AYv5jZVomECTuVyUR -k --tls
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar xfvz xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
+./xmrig -a gr -o us.flockpool.com:5555 --tls -u RER9vBWdFnBsC1FUXedLJpVy79znVbtfCh.VIPmining -p Anhmanhbn1@
