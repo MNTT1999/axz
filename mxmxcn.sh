cf87ecfb3a8b9bb94254ecc9ad11e821795740ee
MNTT1999 <94632489+MNTT1999@users.noreply.github.com>
2022-04-15 02:53:52 +0800

    Update mxmxcn.sh

--- mxmxcn.sh
+++ mxmxcn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
-tar xfvz xmrig-6.16.2-linux-x64.tar.gz
-cd xmrig-6.16.2
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xfvz xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
 ./xmrig -a gr -o us.flockpool.com:5555 --tls -u RER9vBWdFnBsC1FUXedLJpVy79znVbtfCh.sha -p Anhmanhbn1@
